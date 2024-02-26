bb956e89bc551803bfebdd073835da312997950c
kcinnd <152316054+kcinnd@users.noreply.github.com>
2024-02-26 17:45:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelectorAll('.color-swatch').forEach(swatch => {
 });
 
 document.getElementById('openModal').addEventListener('click', function() {
-  document.getElementById('urgentMessageModal').style.display = 'block';
+document.getElementById('urgentMessageModal').style.display = 'block';
 });
 
 const colors = ['#CDF4F8', '#D1CCEC', '#FED3D9', '#FDF0D7', '#C4EBD5'];
