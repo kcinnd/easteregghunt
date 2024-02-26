07e8c4e17375568f888ca4decca45c9ec042200e
kcinnd <152316054+kcinnd@users.noreply.github.com>
2024-02-26 16:12:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -33,8 +33,13 @@ function setupModal() {
   const btn = document.getElementById('openModal');
   const span = document.querySelector('.close');
 
-  btn.addEventListener('click', () => modal.style.display = 'block');
-  span.addEventListener('click', () => modal.style.display = 'none');
+  btn.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
+
+  span.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
 
   window.addEventListener('click', (event) => {
     if (event.target === modal) {
