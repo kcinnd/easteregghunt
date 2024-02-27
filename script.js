3dea1a286e477302bec6db18e9f03ce9ce58309e
kcinnd <152316054+kcinnd@users.noreply.github.com>
2024-02-26 20:41:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-// Utility functions
+var modal = document.getElementById('urgentMessageModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('openModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 function applyColorToEgg(color) {
   const egg = document.getElementById('egg');
   egg ? egg.style.backgroundColor = color : console.error('Egg element not found!');
